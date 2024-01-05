afcda01f6fba91d908195a2d2711998140db3e59
mary-ue <maryandreeva100@gmail.com>
2024-01-05 15:56:18 +0300

    add getIpAddress

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 import { financeControl } from './financeControl.js';
 import { reportControl } from './reportControl.js';
 
-const init = () => {
+// const API_KEY = 'b零fc四零六d二cfecf';
+
+// const getIpAddress = async () => {
+//   const apiUrl = `https://ipinfo.io/json?token=${API_KEY}`;
+
+//   try {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json();
+//     console.log('IP-адрес пользователя:', data.ip);
+//     console.log('Местоположение пользователя:', data.city, data.region, data.country);
+//   } catch (error) {
+//     console.error('Ошибка получения данных:', error);
+//   }
+// };
+
+const init = async () => {
+  // await getIpAddress();
   financeControl();
   reportControl();
 };
